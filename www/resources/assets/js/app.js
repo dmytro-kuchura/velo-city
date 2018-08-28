c64ef9698728eaa3fe27499d86e0e117b7f76811
Dmitry Kuchura <kuchura.d@gmail.com>
2018-08-28 23:05:42 +0300

    add styles

--- www/resources/assets/js/app.js
+++ www/resources/assets/js/app.js
@@ -7,7 +7,11 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+require('./components/menu');
+require('./components/simplecart');
+require('./components/easydropdown');
+
+// window.Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,8 +19,10 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+// const app = new Vue({
+//     el: '#app'
+// });
 
-const app = new Vue({
-    el: '#app'
+$(document).ready(function () {
+    $(".memenu").memenu();
 });
--- www/webpack.mix.js
+++ www/webpack.mix.js
@@ -11,5 +11,9 @@ let mix = require('laravel-mix');
  |
  */
 
+mix.autoload({
+    jquery: ['$', 'window.jQuery', 'jQuery', 'window.$', 'jquery', 'window.jquery']
+});
+
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css');
