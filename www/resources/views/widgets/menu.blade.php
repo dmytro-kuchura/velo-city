5995560f89d973a9269a9e33730ba77b60ea6e9d
Dmitry Kuchura <kuchura.d@gmail.com>
2018-08-29 20:52:22 +0300

    complete menu

--- www/resources/views/widgets/menu.blade.php
+++ www/resources/views/widgets/menu.blade.php
@@ -11,6 +11,7 @@
                             <div class="mepanel">
                                 <div class="row">
                                     <div class="col1 me-one">
+                                        <h4>Велосипеды</h4>
                                         <ul>
                                             <li><a href="products.html">Горные</a></li>
                                             <li><a href="products.html">Городские</a></li>
@@ -25,95 +26,93 @@
                                 </div>
                             </div>
                         </li>
-                        <li class="grid"><a href="#">Women</a>
+                        <li class="grid"><a href="#">Компоненты</a>
                             <div class="mepanel">
                                 <div class="row">
                                     <div class="col1 me-one">
-                                        <h4>Shop</h4>
+                                        <h4>Колесные части</h4>
                                         <ul>
-                                            <li><a href="products.html">New Arrivals</a></li>
-                                            <li><a href="products.html">Blazers</a></li>
-                                            <li><a href="products.html">Swem Wear</a></li>
-                                            <li><a href="products.html">Accessories</a></li>
-                                            <li><a href="products.html">Handbags</a></li>
-                                            <li><a href="products.html">T-Shirts</a></li>
-                                            <li><a href="products.html">Watches</a></li>
-                                            <li><a href="products.html">My Shopping Bag</a></li>
+                                            <li><a href="products.html">Втулки</a></li>
+                                            <li><a href="products.html">Камеры</a></li>
+                                            <li><a href="products.html">Обода</a></li>
+                                            <li><a href="products.html">Покрышки</a></li>
+                                            <li><a href="products.html">Спицы и нипеля</a></li>
+                                            <li><a href="products.html">Ободные ленты</a></li>
+                                            <li><a href="products.html">Колеса в сборе</a></li>
                                         </ul>
                                     </div>
                                     <div class="col1 me-one">
-                                        <h4>Style Zone</h4>
+                                        <h4>Трансмисия и тормоза</h4>
                                         <ul>
-                                            <li><a href="products.html">Shoes</a></li>
-                                            <li><a href="products.html">Watches</a></li>
-                                            <li><a href="products.html">Brands</a></li>
-                                            <li><a href="products.html">Coats</a></li>
-                                            <li><a href="products.html">Accessories</a></li>
-                                            <li><a href="products.html">Trousers</a></li>
+                                            <li><a href="products.html">Каретки</a></li>
+                                            <li><a href="products.html">Касеты/Трещетки</a></li>
+                                            <li><a href="products.html">Педали</a></li>
+                                            <li><a href="products.html">Переключатели</a></li>
+                                            <li><a href="products.html">Цепи и замки</a></li>
+                                            <li><a href="products.html">Шатуны</a></li>
+                                            <li><a href="products.html">Звезды шатунов</a></li>
+                                            <li><a href="products.html">Тормоза (тормозные части)</a></li>
                                         </ul>
                                     </div>
                                     <div class="col1 me-one">
-                                        <h4>Popular Brands</h4>
+                                        <h4>Рамы, вилки, седла, рули</h4>
                                         <ul>
-                                            <li><a href="products.html">499 Store</a></li>
-                                            <li><a href="products.html">Fastrack</a></li>
-                                            <li><a href="products.html">Casio</a></li>
-                                            <li><a href="products.html">Fossil</a></li>
-                                            <li><a href="products.html">Maxima</a></li>
-                                            <li><a href="products.html">Timex</a></li>
-                                            <li><a href="products.html">TomTom</a></li>
-                                            <li><a href="products.html">Titan</a></li>
+                                            <li><a href="products.html">Рамы</a></li>
+                                            <li><a href="products.html">Седла</a></li>
+                                            <li><a href="products.html">Вилки и запчасти</a></li>
+                                            <li><a href="products.html">Подседельные хомуты</a></li>
+                                            <li><a href="products.html">Подседельные штыри</a></li>
+                                            <li><a href="products.html">Рули</a></li>
+                                            <li><a href="products.html">Грипсы</a></li>
+                                            <li><a href="products.html">Выносы</a></li>
                                         </ul>
                                     </div>
                                 </div>
                             </div>
                         </li>
-                        <li class="grid"><a href="#">Kids</a>
+                        <li class="grid"><a href="#">Экипировка и инструменты</a>
                             <div class="mepanel">
                                 <div class="row">
                                     <div class="col1 me-one">
-                                        <h4>Shop</h4>
+                                        <h4>Экипировка</h4>
                                         <ul>
-                                            <li><a href="products.html">New Arrivals</a></li>
-                                            <li><a href="products.html">Blazers</a></li>
-                                            <li><a href="products.html">Swem Wear</a></li>
-                                            <li><a href="products.html">Accessories</a></li>
-                                            <li><a href="products.html">Handbags</a></li>
-                                            <li><a href="products.html">T-Shirts</a></li>
-                                            <li><a href="products.html">Watches</a></li>
-                                            <li><a href="products.html">My Shopping Bag</a></li>
+                                            <li><a href="products.html">Шлемы</a></li>
+                                            <li><a href="products.html">Перчатки</a></li>
+                                            <li><a href="products.html">Очки</a></li>
+                                            <li><a href="products.html">Одежда</a></li>
+                                            <li><a href="products.html">Обувь</a></li>
+                                            <li><a href="products.html">Защита</a></li>
                                         </ul>
                                     </div>
                                     <div class="col1 me-one">
-                                        <h4>Style Zone</h4>
+                                        <h4>Инструменты</h4>
                                         <ul>
-                                            <li><a href="products.html">Shoes</a></li>
-                                            <li><a href="products.html">Watches</a></li>
-                                            <li><a href="products.html">Brands</a></li>
-                                            <li><a href="products.html">Coats</a></li>
-                                            <li><a href="products.html">Accessories</a></li>
-                                            <li><a href="products.html">Trousers</a></li>
+                                            <li><a href="products.html">Выжимки цепей</a></li>
+                                            <li><a href="products.html">Ключи</a></li>
+                                            <li><a href="products.html">Измерительные</a></li>
+                                            <li><a href="products.html">Съемники</a></li>
+                                            <li><a href="products.html">Шестигранники</a></li>
+                                            <li><a href="products.html">Рем. комплекты</a></li>
                                         </ul>
                                     </div>
                                     <div class="col1 me-one">
-                                        <h4>Popular Brands</h4>
+                                        <h4>Аксесуары</h4>
                                         <ul>
-                                            <li><a href="products.html">499 Store</a></li>
-                                            <li><a href="products.html">Fastrack</a></li>
-                                            <li><a href="products.html">Casio</a></li>
-                                            <li><a href="products.html">Fossil</a></li>
-                                            <li><a href="products.html">Maxima</a></li>
-                                            <li><a href="products.html">Timex</a></li>
-                                            <li><a href="products.html">TomTom</a></li>
-                                            <li><a href="products.html">Titan</a></li>
+                                            <li><a href="products.html">Антипрокольная жидкость</a></li>
+                                            <li><a href="products.html">Фляги</a></li>
+                                            <li><a href="products.html">Флягодержатели</a></li>
+                                            <li><a href="products.html">Велокомпьютеры</a></li>
+                                            <li><a href="products.html">Фары, мигалки</a></li>
+                                            <li><a href="products.html">Сумки</a></li>
+                                            <li><a href="products.html">Насосы</a></li>
+                                            <li><a href="products.html">Крылья</a></li>
                                         </ul>
                                     </div>
                                 </div>
                             </div>
                         </li>
-                        <li class="grid"><a href="typo.html">Blog</a>
-                        </li>
-                        <li class="grid"><a href="contact.html">Contact</a>
+                        <li class="grid"><a href="typo.html">Сервис</a></li>
+                        <li class="grid"><a href="contact.html">Контакты</a>
                         </li>
                     </ul>
                 </div>
