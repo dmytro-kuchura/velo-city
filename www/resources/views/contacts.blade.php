dcf6b69330cf69eb26d02ff3c866df946335cc69
Dmitry Kuchura <kuchura.d@gmail.com>
2018-08-29 22:30:17 +0300

    add route

--- www/resources/views/contacts.blade.php
+++ www/resources/views/contacts.blade.php
@@ -47,7 +47,6 @@
     </div>
 
     <div class="map">
-        <iframe src="https://www.google.com/maps/embed/v1/place?q=VELO-CITY&key=AIzaSyC-k1YsWZWfGELhxXsLiHp4QAdUy5P-vBc" style="border:0"></iframe>
-        {{--<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6632.248000703498!2d151.265683!3d-33.7832959!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12abc7edcbeb07%3A0x5017d681632bfc0!2sManly+Vale+NSW+2093%2C+Australia!5e0!3m2!1sen!2sin!4v1433329298259" style="border:0"></iframe>--}}
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6632.248000703498!2d151.265683!3d-33.7832959!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12abc7edcbeb07%3A0x5017d681632bfc0!2sManly+Vale+NSW+2093%2C+Ukraine!5e0!3m2!1sen!2sin!4v1433329298259" style="border:0"></iframe>
     </div>
 @endsection
