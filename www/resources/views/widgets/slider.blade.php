a378bf6082dfcf28b9599d6966eaa434c9c9ac15
Dmitry Kuchura <kuchura.d@gmail.com>
2018-09-05 23:00:33 +0300

    add slider

--- www/resources/views/widgets/slider.blade.php
+++ www/resources/views/widgets/slider.blade.php
@@ -1,17 +1,16 @@
 <div class="image-slider">
     <div class="swiper-wrapper">
+
         <div class="swiper-slide slide">
-            <a href="#"><img class="swiper-lazy" data-src="pic/slider-image1.png" alt=""></a>
+            <a href="#"><img class="swiper-lazy" data-src="/images/slider-1.png" alt=""></a>
             <div class="swiper-lazy-preloader"></div>
         </div>
+
         <div class="swiper-slide slide">
-            <a href="#"><img class="swiper-lazy" data-src="" alt=""></a>
+            <a href="#"><img class="swiper-lazy" data-src="/images/slider-2.png" alt=""></a>
             <div class="swiper-lazy-preloader"></div>
         </div>
-        <div class="swiper-slide slide">
-            <a href="#"><img class="swiper-lazy" data-src="http://imgsrc.me/1366x500/000fff/000000" alt=""></a>
-            <div class="swiper-lazy-preloader"></div>
-        </div>
+
     </div>
     <div id="image-sliderprev" class="prev"></div>
     <div id="image-slidernext" class="next"></div>
