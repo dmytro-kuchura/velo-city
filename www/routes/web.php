b5a3096041383f9af3aac91ba0050f38144acc2d
Dmitry Kuchura <kuchura.d@gmail.com>
2018-09-12 00:03:48 +0300

    add routes

--- www/routes/web.php
+++ www/routes/web.php
@@ -20,4 +20,24 @@ Route::prefix('admin')->group(function () {
     Route::get('/reset-password', 'AdminController@reset')->name('reset');
 
     Route::get('/dashboard', 'AdminController@dashboard')->name('dashboard');
+
+    Route::prefix('pages')->group(function () {
+        Route::get('/', 'PagesController@index')->name('pages.index');
+        Route::get('/create', 'PagesController@create')->name('pages.create');
+        Route::post('/store', 'PagesController@store')->name('pages.store');
+        Route::get('/edit/{id}', 'PagesController@edit')->name('pages.edit');
+        Route::post('/edit', 'PagesController@update')->name('pages.update');
+        Route::get('/delete/{id}', 'PagesController@delete')->name('pages.delete');
+        Route::get('/status/{id}', 'PagesController@status')->name('pages.status');
+    });
+
+    Route::prefix('news')->group(function () {
+        Route::get('/', 'NewsController@index')->name('news.index');
+        Route::get('/create', 'NewsController@create')->name('news.create');
+        Route::post('/store', 'NewsController@store')->name('news.store');
+        Route::get('/edit/{id}', 'NewsController@edit')->name('news.edit');
+        Route::post('/edit', 'NewsController@update')->name('news.update');
+        Route::get('/delete/{id}', 'NewsController@delete')->name('news.delete');
+        Route::get('/status/{id}', 'NewsController@status')->name('news.status');
+    });
 });
