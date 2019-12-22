d7d8e09876efd3876091c66114099d0221d0153b
Dmytro Kuchura <dmytro.kuchura@amadeus.com>
2019-12-23 00:22:42 +0200

    add log

---
+++ src/database/migrations/2019_12_22_111746_create_logs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('name');
+            $table->string('link')->nullable();
+            $table->tinyInteger('deleted')->default(0);
+            $table->string('type')->nullable();
+            $table->string('status')->default(1);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
